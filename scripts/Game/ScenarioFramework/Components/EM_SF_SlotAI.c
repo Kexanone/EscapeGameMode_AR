b81e288b36897aba031b6be1e8eaf4200604b514
Kex <kexanone@gmail.com>
2023-05-07 13:03:29 +0200

    Fixes and clean-up for experimental

--- scripts/Game/ScenarioFramework/Components/EM_SF_SlotAI.c
+++ scripts/Game/ScenarioFramework/Components/EM_SF_SlotAI.c
@@ -8,17 +8,9 @@ class EM_SF_SlotAI : SCR_ScenarioFrameworkSlotAI
 	[Attribute("Column", UIWidgets.EditBox, "Name of the group formation", "")]
 	protected string m_sFormationName;
 
-	override protected void CreateAIGroup()
+	override protected void SetWPGroup()
 	{
-		EntitySpawnParams paramsPatrol = new EntitySpawnParams();
- 		paramsPatrol.TransformMode = ETransformMode.WORLD;
-		paramsPatrol.Transform[3] = m_Entity.GetOrigin();
-		m_AIGroup = SCR_AIGroup.Cast(GetGame().SpawnEntityPrefab(Resource.Load(m_sGroupPrefab) , null, paramsPatrol));
-		FactionAffiliationComponent facComp= FactionAffiliationComponent.Cast(m_Entity.FindComponent(FactionAffiliationComponent));
-		if (!facComp)
-			return;
-		m_AIGroup.SetFaction(facComp.GetAffiliatedFaction());
-		m_AIGroup.AddAIEntityToGroup(m_Entity, 0);
+		super.SetWPGroup();
 		
 		AIFormationComponent formationHandler = AIFormationComponent.Cast(m_AIGroup.FindComponent(AIFormationComponent));
 		
