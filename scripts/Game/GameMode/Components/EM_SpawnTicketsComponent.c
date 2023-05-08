e422fa7d864300fd7d774130430dde692b160f14
Kex <kexanone@gmail.com>
2023-05-08 22:21:35 +0200

    Add spawn tickets

---
+++ scripts/Game/GameMode/Components/EM_SpawnTicketsComponent.c
@@ -0,0 +1,89 @@
+/*
+	This component deletes the initial spawn point if a player is far enough from it
+*/
+
+class EM_SpawnTicketsComponentClass : ScriptComponentClass
+{
+};
+
+class EM_SpawnTicketsComponent : ScriptComponent
+{
+	[Attribute("-1", UIWidgets.EditBox, "", "")]
+	protected int m_iInitialTicketsCount;
+
+	[Attribute("1", desc: "Scenario header setting will overwrite these values.")];
+	protected bool m_bAllowHeaderSettings;
+	
+	[RplProp()]
+	protected int m_iTicketsCount;
+	
+	override void OnPostInit(IEntity owner)
+	{
+		if (!GetGame().InPlayMode())
+			return;
+		
+		SetEventMask(owner, EntityEvent.INIT);
+		owner.SetFlags(EntityFlags.ACTIVE, true);
+
+		super.OnPostInit(owner);
+	};
+	
+	override void EOnInit(IEntity owner)
+	{
+		super.EOnInit(owner);
+		
+		// Initialie tickets
+		m_iTicketsCount = m_iInitialTicketsCount;
+		if (m_bAllowHeaderSettings)
+		{
+			EM_MissionHeaderEscape header = EM_MissionHeaderEscape.Cast(GetGame().GetMissionHeader());
+			
+			if (header)
+				m_iTicketsCount = header.m_iRespawnTickets;
+		};
+		
+		// Tickets disabled
+		if (m_iTicketsCount < 0)
+			return;
+
+		// Run handler only on authority		
+		if (!EM_Utils.IsAuthority(owner))
+			return;
+		
+		SCR_BaseGameMode gameMode = SCR_BaseGameMode.Cast(GetOwner());
+		
+		if (!gameMode)
+			return;
+		
+		gameMode.GetOnPlayerSpawned().Insert(OnPlayerSpawned);
+	};
+	
+	void OnPlayerSpawned(int playerId, IEntity controlledEntity)
+	{
+		ConsumeTicket();
+	};
+	
+	void ConsumeTicket()
+	{
+		m_iTicketsCount -= 1;
+		Replication.BumpMe();
+		
+		if (m_iTicketsCount == 0)
+			DisableSpawn();
+	};
+	
+	void DisableSpawn()
+	{
+		SCR_RespawnSystemComponent respawnSystem = SCR_RespawnSystemComponent.GetInstance();
+		
+		if (!respawnSystem)
+			return;
+		
+		respawnSystem.ServerSetEnableRespawn(false);
+	};
+	
+	int GetTicketsCount()
+	{
+		return m_iTicketsCount;
+	};
+};
---
+++ scripts/Game/UI/Menu/GameMode/modded/SCR_SelectSpawnPointSubMenu.c
@@ -0,0 +1,28 @@
+modded class SCR_SelectSpawnPointSubMenu : SCR_RespawnSubMenuBase
+{
+	protected Widget m_wTicketsCount;
+	protected TextWidget m_wTickets;
+
+	override void GetWidgets()
+	{
+		super.GetWidgets();
+		m_wTicketsCount = GetRootWidget().FindAnyWidget("TicketsCount");
+		if (m_wTicketsCount)
+		{
+			m_wTickets = TextWidget.Cast(m_wTicketsCount.FindAnyWidget("Tickets"));
+		};
+	};
+
+	override void OnMenuUpdate(SCR_SuperMenuBase parentMenu, float tDelta)
+	{
+		super.OnMenuUpdate(parentMenu, tDelta);
+		
+		EM_SpawnTicketsComponent spawnTickets = EM_SpawnTicketsComponent.Cast(GetGame().GetGameMode().FindComponent(EM_SpawnTicketsComponent));
+		
+		if (spawnTickets)
+		{
+			m_wTicketsCount.SetVisible(true);
+			m_wTickets.SetText(spawnTickets.GetTicketsCount().ToString());
+		};
+	};
+};
